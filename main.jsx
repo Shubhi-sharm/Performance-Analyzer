b99510201c9a9e3b3e9fe62e2c992b3d6d79b54d
Shubhi Sharma <shubhisharma1909@gmail.com>
2026-06-20 13:11:09 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { I18nProvider } from "./context/I18nContext.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <I18nProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </I18nProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
